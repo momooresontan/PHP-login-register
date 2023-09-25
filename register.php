f8e46fcec950616ad100a34874511167b6ea844d
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-09-25 13:09:21 +0100

    registration form modified with css

--- register.php
+++ register.php
@@ -4,25 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Form</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" 
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
         <form action="register.php" method="POST">
             <div class="form-group">
-                <input type="text" name="fullname" placeholder="Full Name: ">
+                <input type="text" class="form-control" name="fullname" placeholder="Full Name: ">
             </div>
             <div class="form-group">
-                <input type="email" name="email" placeholder="Email Address: ">
+                <input type="email" class="form-control" name="email" placeholder="Email Address: ">
             </div>
             <div class="form-group">
-                <input type="password" name="password" placeholder="Password: ">
+                <input type="password" class="form-control" name="password" placeholder="Password: ">
             </div>
             <div class="form-group">
-                <input type="password" name="confirm_password" placeholder="Confirm Password">
+                <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password: ">
             </div>
-            <div class="form-group">
-                <input type="submit" value="Register" name="submit">
+            <div class="form-btn">
+                <input type="submit" class="btn btn-primary" value="Register" name="submit">
             </div>
         </form>
     </div>
