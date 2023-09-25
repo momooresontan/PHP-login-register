11b917763d43640596dbf7f5e0fb8efacaa8f33a
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-09-25 15:05:25 +0100

    login form modified(getting email)

--- login.php
+++ login.php
@@ -12,7 +12,12 @@
     <div class="container">
         <?php 
             if(isset($_POST["login"])){
-
+                $email = $_POST["email"];
+                $password = $_POST["password"];
+                require_once "database.php";
+                $sql = "SELECT * FROM users WHERE email = '$email'";
+                $result = mysqli_query($conn, $sql);
+                $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
             }
         ?>
         <form action="login.php" method="post">
