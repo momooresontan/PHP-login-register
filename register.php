9918cd31141f947990a82e2f3834e08152ced21f
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-09-25 14:12:42 +0100

    insert into db

--- register.php
+++ register.php
@@ -40,6 +40,11 @@
                     } else{
                         require_once "database.php";
                         $sql = "INSERT INTO users (full_name, email, password) VALUES (?, ?, ?)";
+                        $stmt = mysqli_stmt_init($conn);
+                        $prepareStmt = mysqli_stmt_prepare($stmt, $sql);
+                        if($prepareStmt){
+                            mysqli_stmt_bind_param($stmt, "sss",$fullname, $email, $passwordHash);
+                        }
                     }
                 }
             ?>
